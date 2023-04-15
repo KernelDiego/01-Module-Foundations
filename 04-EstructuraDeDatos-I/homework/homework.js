700fda4f49726c7f9ec8fdb837a0f2b804d37401
DiegoKernel <gonzalezdiego.contact@gmail.com>
2023-04-15 16:47:51 -0300

    perf: :zap: exercise 4 solved

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,36 +1,26 @@
 'use strict'
 
-/*
-Definir las funciones recursivas nFactorial y nFibonacci.
-
-nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
-
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
-Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
-
-Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
-
-
-Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
-*/
-
-function nFactorial(n) {
+// EJERCICIO 1
+const nFactorial = (n) => {
+  if (n === 1 || n === 0) return 1
+  else if (n < 0) return 0
+  return n * nFactorial(n - 1)
 }
 
-function nFibonacci(n) {
-}
+// EJERCICIO 2
+const nFibonacci = (n) =>  n < 2 ? n : nFibonacci(n - 1) + nFibonacci(n - 2)
 
-/*
-Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
-  - enqueue: agrega un valor respetando el orden.
-  - dequeue: remueve un valor respetando el orden. Retorna undefined cuando la queue está vacía.
-  - size: retorna el tamaño (cantidad de elementos) de la queue.
+// EJERCICIO 3
+// Implementa la clase Queue
+// enqueue:   Agrega un valor a la queue.   Respeta el orden existente.
+// dequeue:   Remueve un valor de la queue.   Obedece a FIFO y respeta el underflow (cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
+// size:   Devuelve el número de elementos que contiene la queue.
 
-Pueden utilizar class o función constructora.
-*/
-
-function Queue() {
-
+const Queue = () => {
+  const array = []
+  this.size = () => array.length
+  this.enqueue = element => array.push(element)
+  this.dequeue = element => array.shift(element)
 }
 
 // No modifiquen nada debajo de esta linea
@@ -39,5 +29,5 @@ function Queue() {
 module.exports = {
   Queue,
   nFactorial,
-  nFibonacci
-};
+  nFibonacci,
+}

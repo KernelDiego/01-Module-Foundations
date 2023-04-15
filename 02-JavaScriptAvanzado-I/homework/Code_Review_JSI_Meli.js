d644da5a5f401631968bab22727319a36d3dda80
DiegoKernel <gonzalezdiego.contact@gmail.com>
2023-04-15 16:41:37 -0300

    perf: :zap: exercise 2 solved

--- 02-JavaScriptAvanzado-I/homework/Code_Review_JSI_Meli.js
+++ 02-JavaScriptAvanzado-I/homework/Code_Review_JSI_Meli.js
@@ -4,30 +4,30 @@
 // Se recomienda siempre declarar las variables, sin importar si están en una función o un ámbito global. Y en el modo estricto (strict mode) de ECMAScript 5, asignar valor a una variable sin declarar lanzará un error.
 
 
-x = 1; //lo asigna global
-var a = 5;
-var b = 10;
+x = 1 //lo asigna global
+var a = 5
+var b = 10
 var c = function(a, b, c) { //El pasaje acá es por referencia o por valor?? por valor, porque es primitivo. hacemos una copia
     //Es lo mismo que tener
     //a = 8
     //b = 9
     //c= 10
-    var x = 10;
-    console.log(x); //10 va a ir a buscar en su contexto, lo mas cercano que tiene
-    console.log(a); //8 
+    var x = 10
+    console.log(x) //10 va a ir a buscar en su contexto, lo mas cercano que tiene
+    console.log(a) //8 
     var f = function(a, b, c) { //declara una funcion y la ejecuta en la linea 24
-        b = a; //valia 9 y lo pisa con a -->  b = 8
-        console.log(b); //8
-        b = c; //10 lo pisa nuevamente
-        var x = 5;
+        b = a //valia 9 y lo pisa con a -->  b = 8
+        console.log(b) //8
+        b = c //10 lo pisa nuevamente
+        var x = 5
         //Lo que hay aca dentro es una copia
     }
-    f(a,b,c); //8 9 10 en su lexical enviroment
-    console.log(b); //cuando llega acá? b se modifica? 9 porque esta fuera de la funcion. cuando es por valor hace una copia
+    f(a,b,c) //8 9 10 en su lexical enviroment
+    console.log(b) //cuando llega acá? b se modifica? 9 porque esta fuera de la funcion. cuando es por valor hace una copia
 }
-c(8,9,10);
-console.log(b); //que lexical enviroment esta buscando?? Aqui vale 10 en su contexto global
-console.log(x); //1 contexto global
+c(8,9,10)
+console.log(b) //que lexical enviroment esta buscando?? Aqui vale 10 en su contexto global
+console.log(x) //1 contexto global
 
 //-------------------------------------------------
 //Hoisting
@@ -36,48 +36,48 @@ console.log(x); //1 contexto global
 //va a subir 
 //var bar
 //funcion foo(){}
-console.log(bar);//undefined
-console.log(baz);//no esta definido
-foo(); //¿Que pasaba con las funciones ?
-function foo() { console.log('Hola!'); }
-var bar = 1;
-baz = 2;
+console.log(bar)//undefined
+console.log(baz)//no esta definido
+foo() //¿Que pasaba con las funciones ?
+function foo() { console.log('Hola!') }
+var bar = 1
+baz = 2
 
 //----------------------------------------
 //let vive entre llaves, entre un bloque de código.
-var instructor = "Tony";
+var instructor = "Tony"
 if(true) { //el if es un statement
-    var instructor = "Franco";
+    var instructor = "Franco"
 }
-console.log(instructor); //Franco, la pisa
+console.log(instructor) //Franco, la pisa
 
 //----------------------------------------------
 
 
 
-var instructor = "Tony";
-console.log(instructor); //Tony
+var instructor = "Tony"
+console.log(instructor) //Tony
 (function() {
     if(true) {
-        var instructor = "Franco";
-        console.log(instructor); //Franco, lo va a ir a buscar a su scope mas cercano
+        var instructor = "Franco"
+        console.log(instructor) //Franco, lo va a ir a buscar a su scope mas cercano
     }
-})(); //IIFE = INMEDIAT INVOC FUNCTION , FUNCION INMEDIATAMENTE INVOCADA, la define y la ejecuta en el momento
-console.log(instructor); //Tony va a buscar el valor en su contexto global
+})() //IIFE = INMEDIAT INVOC FUNCTION , FUNCION INMEDIATAMENTE INVOCADA, la define y la ejecuta en el momento
+console.log(instructor) //Tony va a buscar el valor en su contexto global
 
 
 
 //-------------------------------
-var instructor = "Tony";
-let pm = "Franco";
+var instructor = "Tony"
+let pm = "Franco"
 if (true) {
-    var instructor = "The Flash"; //pisa Tony?? si
-    let pm = "Reverse Flash"; //va a pisar a franco??? vive aca dentro
-    console.log(instructor); //the flash
-    console.log(pm);//Reverse Flash
+    var instructor = "The Flash" //pisa Tony?? si
+    let pm = "Reverse Flash" //va a pisar a franco??? vive aca dentro
+    console.log(instructor) //the flash
+    console.log(pm) //Reverse Flash
 }
-console.log(instructor); //the flash   habia pisado el valor
-console.log(pm);//Franco  No se pisa, busca en el contexto global
+console.log(instructor) //the flash   habia pisado el valor
+console.log(pm) //Franco  No se pisa, busca en el contexto global
 
 
 
@@ -127,56 +127,56 @@ parseInt("09") //9 parsea
 //HOISTING
 
 function test() {
-    //var a; hace hoisnting y sube la declaracion, no su valor, por eso da undefined
-    console.log(a); //existe a?? dentro de las funciones tambien hay hoisting nos da undefined
-    console.log(foo()); //2 cuando hace hoisting, la sube
+    //var a hace hoisnting y sube la declaracion, no su valor, por eso da undefined
+    console.log(a) //existe a?? dentro de las funciones tambien hay hoisting nos da undefined
+    console.log(foo()) //2 cuando hace hoisting, la sube
 
-    var a = 1;
+    var a = 1
     function foo() {
-        return 2;
+        return 2
     }
 }
 
-test();
+test()
 
 
 //----------------------------------------------
 //Se genera hosinting, el if es un statement, tengo un snack dentro de la lista
 //da undefined,
-var snack = 'Meow Mix';
+var snack = 'Meow Mix'
 
 function getFood(food) { //no entra al if, porque por parametro recibe false
     //sube var snack
     if (food) {
-        var snack = 'Friskies';
-        return snack;
+        var snack = 'Friskies'
+        return snack
     }
-    return snack; //va a salir a buscarlo? 
+    return snack //va a salir a buscarlo? 
 }
 
-getFood(false);
+getFood(false)
 
 
 //--------------------------------------
-var fullname = 'Juan Perez';
+var fullname = 'Juan Perez'
 var obj = {
     fullname: 'Natalia Nerea', //una propiedad
     prop: { //otra propiedad que es otro objeto, que tiene una propìedad y un metodo de funcion
         fullname: 'Aurelio De Rosa',
         getFullname: function() {
-            return this.fullname; //este this a que hace referencia? apunta al metodo donde fue creado
+            return this.fullname //este this a que hace referencia? apunta al metodo donde fue creado
             //es lo mismo que diga prop.fullname
         }
     }
-};
+}
 
-console.log(obj.prop.getFullname()); //Aurelio de Rosa
+console.log(obj.prop.getFullname()) //Aurelio de Rosa
 
-var test = obj.prop.getFullname; //Es lo mismo que test= function(){
+var test = obj.prop.getFullname //Es lo mismo que test= function(){
    // return this.fullname  Apunta al objeto global
 //}
 
-console.log(test());
+console.log(test())
 
 //--------------------------------------
 //Asincronico
@@ -197,10 +197,10 @@ console.log(test());
 
 
 function printing() {
-    console.log(1);
-    setTimeout(function() { console.log(2); }, 1000);
-    setTimeout(function() { console.log(3); }, 0);
-    console.log(4);
+    console.log(1)
+    setTimeout(function() { console.log(2) }, 1000)
+    setTimeout(function() { console.log(3) }, 0)
+    console.log(4)
 } 
 
-printing();
+printing()
